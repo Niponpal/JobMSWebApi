0540f47a0ad1360f9981d2822383f48fd531e4fc
Niponpal <niponpal52@gmail.com>
2026-05-11 11:36:05 +0600

    Fix All 🚀🚀🚑

---
+++ Data/Auth/RoleConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using JobMS.Auth_IdentityModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    
+namespace JobMS.Data.Configuration;
+
+public class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.HasData(
+            new Role
+            {
+                Id = 1,
+                Name = "Administrator",
+                NormalizedName = "ADMINISTRATOR",
+                Description = "System Administrator"
+            },
+            new Role
+            {
+                Id = 2,
+                Name = "Employer",
+                NormalizedName = "EMPLOYER",
+                Description = "Employer job postings"
+            },
+            new Role
+            {
+                Id = 3,
+                Name = "Candidate",
+                NormalizedName = "CANDIDATE",
+                Description = "Candidate user"
+            }
+        );
+    }
+}
---
+++ model/Auth/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobMSWebApi.Models.Auth
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        //[Required]
+        //[DataType(DataType.Password)]
+        //[Compare("Password", ErrorMessage = "Password does not match")]
+        //public string ConfirmPassword { get; set; }
+
+        [StringLength(20)]
+        public string PhoneNumber { get; set; }
+
+        public IFormFile ImageFile { get; set; }
+    }
+}
