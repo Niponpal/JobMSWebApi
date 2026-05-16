6fd03efd30ef209b70ffc1bdbcba8240d7c70d35
Niponpal <niponpal52@gmail.com>
2026-05-16 21:40:37 +0600

    Fix 🚑🚑🚀

--- Controllers/JobController.cs
+++ Controllers/JobController.cs
@@ -1,9 +1,11 @@
 ﻿using JobMSWebApi.Repository;
 using JobMSWebApi.ViewModel.Job;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JobMSWebApi.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class JobController : ControllerBase
@@ -37,19 +39,27 @@ public class JobController : ControllerBase
 
     // CREATE
     [HttpPost]
-    public async Task<IActionResult> Create([FromBody] JobCreateDto dto, CancellationToken cancellationToken)
+    public async Task<IActionResult> Create(
+        [FromBody] JobCreateDto dto,
+        CancellationToken cancellationToken)
     {
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
 
         var data = await _repo.AddJobAsync(dto, cancellationToken);
 
-        return CreatedAtAction(nameof(GetById), new { id = data.Id }, data);
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = data.Id },
+            data);
     }
 
     // UPDATE
     [HttpPut("{id}")]
-    public async Task<IActionResult> Update(long id, [FromBody] JobUpdateDto dto, CancellationToken cancellationToken)
+    public async Task<IActionResult> Update(
+        long id,
+        [FromBody] JobUpdateDto dto,
+        CancellationToken cancellationToken)
     {
         if (id != dto.Id)
             return BadRequest(new { message = "Id mismatch" });
@@ -67,7 +77,9 @@ public class JobController : ControllerBase
 
     // DELETE
     [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(long id, CancellationToken cancellationToken)
+    public async Task<IActionResult> Delete(
+        long id,
+        CancellationToken cancellationToken)
     {
         var result = await _repo.DeleteJobAsync(id, cancellationToken);
 
